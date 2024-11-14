6ae580d3b2701c1247c95beeed83eab88c4b0394
cwbeech <cwbeech@k-state.edu>
2024-11-14 16:13:00 -0600

    minor fix

--- GameTracking/Program.cs
+++ GameTracking/Program.cs
@@ -8,9 +8,7 @@ namespace GameTracking
 		[STAThread]
 		static void Main()
 		{
-			// To customize application configuration such as set high DPI settings or default font,
-			// see https://aka.ms/applicationconfiguration.
-			ApplicationConfiguration.Initialize();
+
 		}
 	}
 }
