5b234e32fc2e0c0bcf591450845134f33b7b8497
Froggy1414 <nalenrangarajan@icloud.com>
2024-11-21 22:14:39 -0600

    stuff

---
+++ GameTracking/Sql/Procedures/Get By Unique Procedures/GameTrack.GetReviewByUsernameAndGameID.sql
@@ -0,0 +1,12 @@
+﻿CREATE OR ALTER PROCEDURE GameTrack.GetReviewByUsernameAndGameID
+	@Username NVARCHAR(32),
+	@GameID INT
+AS
+
+SELECT R.ReviewID, R.GameID, R.Score, R.Body, R.ReviewDate
+FROM GameTrack.[Profile] P 
+	INNER JOIN GameTrack.GameProfile GP ON P.ProfileID = GP.ProfileID
+	INNER JOIN GameTrack.Game G ON GP.GameID = G.GameID
+	INNER JOIN Review R ON G.GameID = R.ReviewID
+WHERE P.Username = @Username AND G.GameID = @GameID;
+GO
