a0349ca44a7877d30aa17eb3025b88abcc9042f6
Froggy1414 <nalenrangarajan@icloud.com>
2024-11-22 01:18:30 -0600

    uhbs

---
+++ GameTracking/Sql/Procedures/Aggregating Procedures/GameTrack.GetPercentPositiveReviewsForGame.sql
@@ -0,0 +1,8 @@
+﻿CREATE OR ALTER PROCEDURE GameTrack.GetPercentPositiveReviewsForGame
+	@GameID INT
+AS
+
+SELECT COUNT(DISTINCT R.ReviewID) / COUNT(*) AS PositivePercent
+FROM GameTrack.Game G 
+	INNER JOIN GameTrack.Review R ON G.ReviewID = R.ReviewID
+WHERE
