1c3f11d4d5ce8c7bf4ba5b6eeb46dc398df843c0
Froggy1414 <nalenrangarajan@icloud.com>
2024-11-21 19:27:36 -0600

    procedures fixed

--- GameTracking/Sql/Procedures/Get By Unique Procedures/GameTrack.GetGenreByName.sql
+++ GameTracking/Sql/Procedures/Get By Unique Procedures/GameTrack.GetGenreByName.sql
@@ -1,8 +1,8 @@
-﻿CREATE OR ALTER PROCEDURE GameTrack.FetchGenre
+﻿CREATE OR ALTER PROCEDURE GameTrack.GetGenreByName
 	@Name NVARCHAR(16)
 AS
 
-SELECT G.GenreID, P.[Name] 
+SELECT G.GenreID, G.[Name] 
 FROM GameTrack.Genre G
 WHERE G.[Name] = @Name;
 GO
